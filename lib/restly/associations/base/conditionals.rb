6484377a9d5c587db5e8e7af602297e967420dc4
Jason Waldrip <jason@waldrip.net>
2012-11-19 17:41:11 -0700

    Comment Out Association Conditionals for the Time Being

--- lib/restly/associations/base/conditionals.rb
+++ lib/restly/associations/base/conditionals.rb
@@ -2,8 +2,8 @@ module Restly::Associations::Base::Conditionals
 
   # Conditionals
   def valid?(val)
-    valid_instances = Array.wrap(val).reject{ |item| item.resource_name == association_class.resource_name }.empty?
-    raise Restly::Error::InvalidObject, "#{val} is not a #{association_class}" unless valid_instances
+    #valid_instances = Array.wrap(val).reject{ |item| item.resource_name == association_class.resource_name }.empty?
+    #raise Restly::Error::InvalidObject, "#{val} is not a #{association_class}" unless valid_instances
   end
 
   def collection?
